14f296211a4f5639b45f3f852ac92f671e1fe791
whohas3E <peifenelite@gmail.com>
2022-07-29 18:19:17 +0800

    git fetch

--- js/custom.js
+++ js/custom.js
@@ -1,62 +1,57 @@
 // ACCORDIAN FAQ
 
 const accordianItemHeaders = document.querySelectorAll(
-    ".accordian-item-header"
-  );
-  
-  accordianItemHeaders.forEach(accordianItemHeader => {
-    accordianItemHeader.addEventListener("click", () => {
-      const current = document.querySelector(".accordian-item-header.active");
-  
-      if (current && current !== accordianItemHeader) {
-        current.classList.toggle("active");
-        current.nextElementSibling.style.maxHeight = 0;
-      }
-      accordianItemHeader.classList.toggle("active");
-  
-      const accordianItemBody = accordianItemHeader.nextElementSibling;
-  
-      if (accordianItemHeader.classList.contains("active")) {
-        accordianItemBody.style.maxHeight = accordianItemBody.scrollHeight + "px";
-      } else {
-        accordianItemBody.style.maxHeight = 0;
-      }
-    });
-  });
+	".accordian-item-header"
+);
+
+accordianItemHeaders.forEach((accordianItemHeader) => {
+	accordianItemHeader.addEventListener("click", () => {
+		const current = document.querySelector(".accordian-item-header.active");
+
+		if (current && current !== accordianItemHeader) {
+			current.classList.toggle("active");
+			current.nextElementSibling.style.maxHeight = 0;
+		}
+		accordianItemHeader.classList.toggle("active");
+
+		const accordianItemBody = accordianItemHeader.nextElementSibling;
+
+		if (accordianItemHeader.classList.contains("active")) {
+			accordianItemBody.style.maxHeight =
+				accordianItemBody.scrollHeight + "px";
+		} else {
+			accordianItemBody.style.maxHeight = 0;
+		}
+	});
+});
 
 //   scroll magic on background video
-const hero = document.querySelector('.hero');
-const video = hero.querySelector('.scroll-video');
-const heroText = hero.querySelector('.scroll-text')
+const hero = document.querySelector(".hero");
+const video = hero.querySelector(".scroll-video");
+const heroText = hero.querySelector(".scroll-text");
+const herowWrapper = hero.querySelector(".hero-wrapper");
 
 const controller = new ScrollMagic.Controller();
 
 let scene = new ScrollMagic.Scene({
-  duration: 5000,
-  triggerElement: hero,
-  triggerHook: 0
+	duration: 5000,
+	triggerElement: hero,
+	triggerHook: 0,
 })
-.setPin(hero)
-.addTo(controller);
+	.setPin(hero)
+	.addTo(controller);
 
-const heroTextAnimation = TweenMax.fromTo(heroText, 3, { opacity: 1}, {opacity: 0})
-let scene2= new ScrollMagic.Scene({
-  duration: 1000,
-  triggerElement: hero,
-  triggerHook: 0
-})
-.setTween(heroTextAnimation)
-.addTo(controller)
-
-let accelamount = 0.1
-let scrollpos = 0
-let delay = 0
-
-scene.on("update", e=>{
-  scrollpos = e.scrollPos / 1000;
+const heroTextAnimation = TweenMax.fromTo(
+	heroText,
+	3,
+	{opacity: 1, scale: 1},
+	{opacity: 0, scale: 2}
+);
+let scene2 = new ScrollMagic.Scene({
+	duration: 5000,
+	triggerElement: hero,
+	triggerHook: 0,
 })
 
-setInterval(()=>{
-  delay += (scrollpos - delay) * accelamount
-  video.currentTime = delay
-}, 30)
+	.setTween(heroTextAnimation)
+	.addTo(controller);
